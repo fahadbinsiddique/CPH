a4ca4140954002e0c46ade09d0efcf3d0f55ad0a
Fahad Bin Siddique <fahad.bin.siddique345@gmail.com>
2026-06-08 15:06:29 +0600

    push

--- center-for-psychology-health/app/layout.js
+++ center-for-psychology-health/app/layout.js
@@ -35,7 +35,7 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <Navbar />
-        <main>{children}</main>
+        {children}
         <Toaster/>
         <Footer />
       </body>
